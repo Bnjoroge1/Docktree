4313fd9fe3898c055d2d315f478f8642dc66632e
Bill Njoroge <williamsriunge@gmail.com>
2026-06-30 22:51:21 -0400

    chore: bump version to v0.1.8

--- Formula/docktree.rb
+++ Formula/docktree.rb
@@ -1,25 +1,25 @@
 class Docktree < Formula
   desc "Run Docker Compose services across multiple git worktrees without port conflicts"
   homepage "https://github.com/Bnjoroge1/Docktree"
-  version "0.1.0"
+  version "0.1.8"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Bnjoroge1/Docktree/releases/download/v0.1.0/docktree_0.1.0_darwin_arm64.tar.gz"
+      url "https://github.com/Bnjoroge1/Docktree/releases/download/v0.1.8/docktree_0.1.8_darwin_arm64.tar.gz"
       sha256 "PLACEHOLDER_ARM64_SHA256"
     else
-      url "https://github.com/Bnjoroge1/Docktree/releases/download/v0.1.0/docktree_0.1.0_darwin_amd64.tar.gz"
+      url "https://github.com/Bnjoroge1/Docktree/releases/download/v0.1.8/docktree_0.1.8_darwin_amd64.tar.gz"
       sha256 "PLACEHOLDER_AMD64_SHA256"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Bnjoroge1/Docktree/releases/download/v0.1.0/docktree_0.1.0_linux_arm64.tar.gz"
+      url "https://github.com/Bnjoroge1/Docktree/releases/download/v0.1.8/docktree_0.1.8_linux_arm64.tar.gz"
       sha256 "PLACEHOLDER_LINUX_ARM64_SHA256"
     else
-      url "https://github.com/Bnjoroge1/Docktree/releases/download/v0.1.0/docktree_0.1.0_linux_amd64.tar.gz"
+      url "https://github.com/Bnjoroge1/Docktree/releases/download/v0.1.8/docktree_0.1.8_linux_amd64.tar.gz"
       sha256 "PLACEHOLDER_LINUX_AMD64_SHA256"
     end
   end
@@ -29,6 +29,6 @@ class Docktree < Formula
   end
 
   test do
-    assert_match "0.1.0", shell_output("#{bin}/docktree --version")
+    assert_match "0.1.8", shell_output("#{bin}/docktree --version")
   end
 end
